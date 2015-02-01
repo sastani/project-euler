862f48e2fdb343c890ab75e378264fbd8ccd049b
Sina Astani <sinaastani@gmail.com>
2015-01-31 17:04:13 -0800

    Create ProblemSeven.java

---
+++ Java/ProblemSeven.java
@@ -0,0 +1,22 @@
+public class ProblemSeven{
+	public static void main(String args[]){
+		int x, y, count;
+		x=3; 
+		count=1;
+		while (count<10001)
+		{
+			for (y=2; y<x; y++)
+			{
+			if (x%y==0)
+				break;
+			}
+			if (y==x)
+				count++;
+			if (count==10001)
+				break;
+			else
+				x++;
+		}
+		System.out.println(x);
+	}
+}
