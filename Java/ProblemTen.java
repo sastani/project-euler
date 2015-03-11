b7ae5e713743690c3fea1aea86f9afc2b9f0011d
Sina Astani <sinaastani@gmail.com>
2015-03-10 22:59:18 -0700

    Update ProblemTen.java

--- Java/ProblemTen.java
+++ Java/ProblemTen.java
@@ -1,18 +1,22 @@
-public class ProblemTen{
+public class ProblemTen
+{
 	public static final int PRIMES_BELOW = 2000000;
-	public static void main(String args[]){
+	public static void main(String args[])
+	{
 		long x, y, sum;
 		x=3; 
 		sum=2;
 		while(x<PRIMES_BELOW)
 		{
 		for (y=2; y<x; y++)
-			{if(x%y==0)
+			{
+				if(x%y==0)
 				break;
 			}
 		if(y==x)
-			{sum+=y;
-			 x++;
+			{
+				sum+=y;
+			 	x++;
 			}
 		x++;
 		}
