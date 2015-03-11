cb4de534d0a7addf6f1ca26b014e1003b1ede476
Sina Astani <sinaastani@gmail.com>
2015-03-10 22:49:51 -0700

    Create ProblemFive.java

---
+++ Java/ProblemFive.java
@@ -0,0 +1,30 @@
+public class ProblemFive 
+{
+	public static void main(String[] args) 
+	{
+		int smallest, number;
+		smallest=0;
+		number=1;
+		boolean divisible=false;
+		
+		
+		while (divisible==false)
+		{
+			for(int i=1; i<21; i++)
+			{
+				if (number%i!=0)
+				{
+					number++;
+					break;
+				}
+				if (i==20)
+				{
+					smallest=number;
+					divisible=true;
+					break;
+				}
+			}
+		}
+		System.out.println(smallest);
+	}
+}
