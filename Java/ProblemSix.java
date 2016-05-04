b7aea5870c29de74ae9de6ed457b2ec73405f4b8
Sina Astani <sinaastani@gmail.com>
2016-05-03 21:05:12 -0700

    Update ProblemSix.java

--- Java/ProblemSix.java
+++ Java/ProblemSix.java
@@ -5,13 +5,14 @@ public class ProblemSix{
     int sumofnum=0;
     int square;
     for(x=1;x<=100;x++)
-    {square=x*x;
-    sumofsq+=square;
+    {
+      square=x*x;
+      sumofsq+=square;
     }
     for(y=1;y<=100;y++)
-    {sumofnum+=y;}
-    System.out.println((sumofnum*sumofnum)-sumofsq);
-      
+    {
+      sumofnum+=y;
     }
-       
+    System.out.println((sumofnum*sumofnum)-sumofsq);
+  }
 }
